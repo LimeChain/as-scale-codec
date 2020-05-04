3ddcf96b9c2f379f63fa5b079837745e5c1d2d98
Daniel Ivanov <daniel@limechain.tech>
2020-05-04 17:46:51 +0300

    add initial library structure and simple export of encode/decode

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AssemblyScript SCALE Codec
 AssemblyScript implementation of the SCALE codec used in the Parity Substrate framework.
+
---
+++ tests/index.js
@@ -0,0 +1,6 @@
+const assert = require("assert");
+const { Encoder, Decoder } = require("..");
+assert.ok(Encoder !== undefined);
+assert.ok(Decoder !== undefined);
+
+console.log("Simple export okay");
