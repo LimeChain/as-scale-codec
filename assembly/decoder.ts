6dc7c0157ab36d5f2a74b9cca5c4e385f827fb8e
Daniel Ivanov <daniel@limechain.tech>
2020-05-05 16:33:33 +0300

    add initial bool support

--- assembly/decoder.ts
+++ assembly/decoder.ts
@@ -1,5 +1,21 @@
 export class Decoder {
-    constructor() {
 
+    // TODO
+    // decode (value: u8[]): bool {
+    //     trace(value.length.toString());
+    //     return this.decodeBool(value);
+    // }
+
+    static decodeBool (value: u8[]): bool {
+        if (value.length != 1) {
+            throw new Error('cannot decode invalid boolean');
+        }
+
+        if (value[0] == 1) {
+            return true;
+        } else if (value[0] === 0) {
+            return false;
+        }
+        throw new Error('cannot decode invalid boolean');
     }
 }
--- assembly/encoder.ts
+++ assembly/encoder.ts
@@ -1,5 +1,20 @@
 export class Encoder {
-    constructor() {
 
+    // TODO
+    // encode<T>(value: T): u8[] {
+    //     trace(nameof(value));
+    //     switch (nameof(value)) {
+    //         case "bool":
+    //             return this.encodeBool(true);
+    //         default:
+    //             new Error('unsupported type');
+    //     }
+    //     return [1];
+    // }
+
+    static encodeBool(value: bool): u8[] {
+        var bytesEncoded = new Array<u8>(1);
+        bytesEncoded[0] = value ? 0x01 : 0x00;
+        return bytesEncoded;
     }
 }
