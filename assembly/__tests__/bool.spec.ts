1dac05447065366af4e01697f6b5f7dbe081c3a2
Daniel Ivanov <daniel@limechain.tech>
2020-05-07 15:00:49 +0300

    refactor structure

--- assembly/__tests__/bool.spec.ts
+++ assembly/__tests__/bool.spec.ts
@@ -1,15 +1,15 @@
-import { Bool } from "../bool";
+import { Bool } from "../Bool";
 
 describe("Bool", () => {
 
 	it("should encode bool with true", () => {
 		let v = new Bool(true);
-		expect<u8[]>(v.encode()).toStrictEqual([0x01]);
+		expect<u8[]>(v.toU8a()).toStrictEqual([0x01]);
 	});
 
 	it("should encode bool with false", () => {
 		let v = new Bool(false);
-		expect<u8[]>(v.encode()).toStrictEqual([0x00]);
+		expect<u8[]>(v.toU8a()).toStrictEqual([0x00]);
 	});
 
 	it("should instantiate true Bool from U8Array", () => {
@@ -19,5 +19,13 @@ describe("Bool", () => {
 	it("should instantiate false Bool from U8Array", () => {
 		expect<Bool>(Bool.fromU8a([0x00])).toStrictEqual(new Bool(false));
 	});
+
+	itThrows('when provided invalid bool byte array', () => {
+		Bool.fromU8a([0x00, 0x01, 0xff]);
+	});
+
+	itThrows('when provided invalid bool value', () => {
+		Bool.fromU8a([5]);
+	})
 
 });
--- assembly/bool.ts
+++ assembly/bool.ts
@@ -1,21 +1,44 @@
-import { Encoder } from "./encoder";
-import { Decoder } from "./decoder";
+import { Codec } from "./Codec";
 
 /** Representation for a boolean value in the system. */
-export class Bool {
+export class Bool implements Codec {
 
     constructor(public value: bool) {
         this.value = value;
     }
 
-    /** Encodes the value as Uint8Array as per the SCALE codec specification */
-    encode(): u8[] {
-        return Encoder.encodeBool(this.value);
+    /** Encodes the value as u8[] as per the SCALE codec specification
+     * true -> [1]
+     * false -> [0]
+     */
+    toU8a(): u8[] {
+        let bytesEncoded = new Array<u8>(1);
+        bytesEncoded[0] = this.value ? 0x01 : 0x00;
+        return bytesEncoded;
     }
 
-    /** Instantiates new Bool from Uint8Array SCALE encoded bytes */
+    /**
+     * @description Returns the string representation of the value
+     */
+    toString(): string {
+        return `${this.value}`;
+    }
+
+    /**
+     * @description The length of Uint8Array when the value is encoded
+     */
+    public encodedLength(): i32 {
+        return 1;
+    }
+
+    /** Instantiates new Bool from u8[] SCALE encoded bytes */
     static fromU8a (value: u8[]): Bool {
-        let result = Decoder.decodeBool(value);
-        return new Bool(result);
+        if (value.length != 1) {
+            throw new Error('cannot decode invalid boolean'); // TODO use null
+        }
+        if (value[0] == 1 || value[0] == 0) {
+            return new Bool(value[0] == 1);
+        }
+        throw new Error('cannot decode invalid boolean'); // TODO use null
     }
 }
