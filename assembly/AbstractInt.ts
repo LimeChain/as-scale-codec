48cd50ef13e40f2e08e579c402d8c48b2ca724dd
bakasura980 <lyubo@limechain.tech>
2020-05-12 19:29:01 +0300

    Abstract int impl

--- assembly/AbstractInt.ts
+++ assembly/AbstractInt.ts
@@ -1,7 +1,8 @@
 import { Codec } from "./Codec";
+import { Bytes, BIT_LENGTH } from "./utils/Bytes";
 
 /** Representation for a Int value in the system. */
-export abstract class AbstractInt<T> implements Codec {
+export abstract class AbstractInt<T extends number> implements Codec {
 
     readonly bitLength: i32;
     readonly value: T;
@@ -12,12 +13,23 @@ export abstract class AbstractInt<T> implements Codec {
     }
 
     /** Encodes the value as u8[] as per the SCALE codec specification */
-    abstract toU8a(): u8[]
+    public toU8a (): u8[] {
+        let bytesEncoded = new Array<u8>(this.bitLength);
+        Bytes.putUint<T>(bytesEncoded, this.value, this.bitLength);
+        return bytesEncoded;
+    }
+
+    /**
+     * @description Returns the string representation of the value
+     */
+    toString (): string {
+        return this.value.toString();
+    }
 
     /**
      * @description The length of Uint8Array when the value is encoded
      */
-    public encodedLength(): i32 {
+    public encodedLength (): i32 {
         return this.bitLength;
     }
 }
--- assembly/utils/Bytes.ts
+++ assembly/utils/Bytes.ts
@@ -5,42 +5,23 @@ export const enum BIT_LENGTH {
     INT_64 = 8
 }
 
+
 export class Bytes {
 
-    static putUint16(b: u8[], v: u16): void {
+    static putUint<T extends number> (b: u8[], v: T, bitLength: number): void {
         b[0] = u8(v);
-        b[1] = u8(v >> 8);
+
+        for (let i: u8 = 1; i < bitLength; i++) {
+            b[i] = u8(v >> i * 8);
+        }
     }
 
-    static putUint32(b: u8[], v: u32): void {
-        b[0] = u8(v);
-        b[1] = u8(v >> 8);
-        b[2] = u8(v >> 16);
-        b[3] = u8(v >> 24)
-    }
+    static toUint<T extends number> (b: u8[], bitLength: number): T {
+        let result: T = <T>b[0];
+        for (let i: u8 = 1; i < bitLength; i++) {
+            result |= (b[i] as T) << 8 * i;
+        }
 
-    static putUint64(b: u8[], v: u64): void {
-        b[0] = u8(v);
-        b[1] = u8(v >> 8);
-        b[2] = u8(v >> 16);
-        b[3] = u8(v >> 24);
-        b[4] = u8(v >> 32);
-        b[5] = u8(v >> 40);
-        b[6] = u8(v >> 48);
-        b[7] = u8(v >> 56)
+        return result;
     }
-
-    static toUint16(b: u8[]): u16 {
-        return u16(b[0]) | u16(b[1]) << 8
-    }
-
-    static toUint32(b: u8[]): u32 {
-        return b[0] | u32(b[1]) << 8 | u32(b[2]) << 16 | u32(b[3]) << 24
-    }
-
-    static toUint64(b: u8[]): u64 {
-        return u64(b[0]) | u64(b[1]) << 8 | u64(b[2]) << 16 | u64(b[3]) << 24 |
-            u64(b[4]) << 32 | u64(b[5]) << 40 | u64(b[6]) << 48 | u64(b[7]) << 56
-    }
-
 }
