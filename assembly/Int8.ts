d5bba20447f23545bb4e5f9b0457f27490ed73ca
bakasura980 <lyubo@limechain.tech>
2020-05-13 10:19:08 +0300

    Remove toU8a from int8

--- assembly/Int8.ts
+++ assembly/Int8.ts
@@ -8,14 +8,6 @@ export class Int8 extends AbstractInt<i8> {
         super(value, BIT_LENGTH.INT_8)
     }
 
-    /** Encodes the value as u8[] as per the SCALE codec specification */
-    toU8a (): u8[] {
-        let bytesEncoded = new Array<u8>(1); //Bytes length / 8
-        bytesEncoded[0] = this.value;
-        return bytesEncoded;
-    }
-
-
     /** Instantiates new Bool from u8[] SCALE encoded bytes */
     static fromU8a (value: u8[]): Int8 {
         if (value.length != 1) {
