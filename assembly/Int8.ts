ec13933ce42864aaedd40b7116794da36b1b3654
bakasura980 <lyubo@limechain.tech>
2020-05-13 10:38:33 +0300

    Merge with master

--- assembly/Int8.ts
+++ assembly/Int8.ts
@@ -4,35 +4,8 @@ import { BIT_LENGTH } from "./utils/Bytes";
 /** Representation for a Int8 value in the system. */
 export class Int8 extends AbstractInt<i8> {
 
-<<<<<<< HEAD
-    constructor(public value: i8) {
-        this.value = value;
-    }
-
-    /** Encodes the value as u8[] as per the SCALE codec specification */
-    toU8a (): u8[] {
-        if (this.value < 1 << 6) {
-
-        }
-        return [];
-    }
-
-    /**
-     * @description Returns the string representation of the value
-     */
-    toString (): string {
-        return this.value.toString();
-    }
-
-    /**
-     * @description The length of Uint8Array when the value is encoded
-     */
-    public encodedLength (): i32 {
-        return 1;
-=======
     constructor(value: i8) {
         super(value, BIT_LENGTH.INT_8)
->>>>>>> 6b60df31fe8d87ded6a01890a5575a8333c8646f
     }
 
     /** Instantiates new Bool from u8[] SCALE encoded bytes */
--- assembly/utils/Bytes.ts
+++ assembly/utils/Bytes.ts
@@ -8,34 +8,6 @@ export const enum BIT_LENGTH {
 
 export class Bytes {
 
-<<<<<<< HEAD
-    static toUint (b: u8[], bytesLength: u32): void {
-        // TODO
-    }
-
-    static toUint16 (b: u8[]): u16 {
-        return u16(b[0]) | u16(b[1]) << 8
-    }
-
-    static toUint32 (b: u8[]): u32 {
-        return b[0] | u32(b[1]) << 8 | u32(b[2]) << 16 | u32(b[3]) << 24
-    }
-
-    static toUint64 (b: u8[]): u64 {
-        return u64(b[0]) | u64(b[1]) << 8 | u64(b[2]) << 16 | u64(b[3]) << 24 |
-            u64(b[4]) << 32 | u64(b[5]) << 40 | u64(b[6]) << 48 | u64(b[7]) << 56
-    }
-
-    static copyFromPosition (input: u8[], out: Array<u8>, position: i32 = 0): void {
-        for (let i = 0; i < out.length; i++) {
-            if (input.length <= i) {
-                break;
-            }
-            out[position + i] = input[i];
-        }
-    }
-}
-=======
     static putUint<T extends number> (b: u8[], v: T, bitLength: number): void {
         b[0] = u8(v);
 
@@ -52,5 +24,14 @@ export class Bytes {
 
         return result;
     }
+
+    static copyFromPosition (input: u8[], out: Array<u8>, position: i32 = 0): void {
+        for (let i = 0; i < out.length; i++) {
+            if (input.length <= i) {
+                break;
+            }
+            out[position + i] = input[i];
+        }
+    }
 }
->>>>>>> 6b60df31fe8d87ded6a01890a5575a8333c8646f
+
