eb0f224a1ea043c344a27ad3152dd7b48512d214
bakasura980 <lyubo@limechain.tech>
2020-05-15 13:17:19 +0300

    Implement byte arrays encoding

--- assembly/ByteArray.ts
+++ assembly/ByteArray.ts
@@ -1,25 +1,48 @@
-import { Codec } from "./Codec";
-import { Bytes, BIT_LENGTH } from "./utils/Bytes"
+import { Bytes } from "./utils/Bytes"
 
-export class ByteArray extends Array<u8> implements Codec {
-    constructor(length: i32) {
-        super(length);
+export class ByteArray extends Array<u8> {
+    constructor(input: u8[]) {
+        super(input.length);
+        Bytes.copy(input, this);
     }
 
+    /**
+    * @description  Encodes ByteArray as u8[] as per the SCALE codec specification
+    */
     public toU8a (): u8[] {
-        return [];
+        let bytesBuffer = new Array<u8>(this.length);
+        const encodedBytes: i32 = Bytes.encodeInteger(bytesBuffer, this.length);
+
+        Bytes.copy(this, bytesBuffer, i32(encodedBytes));
+
+        return bytesBuffer;
     }
 
-    public encodedLength (): i32 {
-        return this.length;
+    /**
+    * @description  Return hex representation of ByteArray
+    */
+    public toString (): string {
+        let result = "0x";
+        for (let i = 0; i < this.length; i++) {
+            const str = this[i].toString();
+            if (str.length == 1) {
+                result += "0";
+            }
+
+            result += str;
+        }
+
+        return result;
     }
 
+    /**
+    * @description Instantiates ByteArray from u8[] SCALE encoded bytes (Decode)
+    */
     static fromU8a (input: u8[]): ByteArray {
         const bytesLength = Bytes.decodeInt(input);
-        const bytesArray = new ByteArray(i32(bytesLength));
+        const bytes = input.slice(i32(input.length - bytesLength));
 
-        Bytes.copyFromPosition(input, bytesArray, i32(input.length - bytesLength));
-
-        return bytesArray;
+        return new ByteArray(bytes);
     }
 }
+
--- assembly/utils/Bytes.ts
+++ assembly/utils/Bytes.ts
@@ -25,22 +25,51 @@ export class Bytes {
         return result;
     }
 
-    static copyFromPosition (src: u8[], dst: Array<u8>, position: i32 = 0): void {
+    /**
+    * @description Copy src elements in dst at provided position. 
+    */
+    static copy (src: u8[], dst: Array<u8>, start: i32 = 0): void {
         for (let i = 0; i < dst.length; i++) {
             if (src.length <= i) {
                 break;
             }
-            dst[i] = src[position + i];
+            dst[start + i] = src[i];
         }
     }
 
-    static copyToPosition (src: u8[], dst: Array<u8>, position: i32 = 0): void {
-        for (let i = 0; i < dst.length; i++) {
-            if (src.length <= i) {
-                break;
-            }
-            dst[position + i] = src[i];
+    static encodeInteger (bytesBuffer: u8[], i: i32): i32 {
+        if (i < 1 << 6) {
+            Bytes.putUint<u8>(bytesBuffer, u8(i) << 2, BIT_LENGTH.INT_8);
+            return 1
         }
+
+        if (i < 1 << 14) {
+            Bytes.putUint<u16>(bytesBuffer, u16(i << 2) + 1, BIT_LENGTH.INT_16);
+            return 2;
+        }
+
+        if (i < 1 << 30) {
+            Bytes.putUint<u32>(bytesBuffer, u32(i << 2) + 2, BIT_LENGTH.INT_32);
+            return 4;
+        }
+
+        const o = new Array<u8>(8);
+        let m = i;
+
+        let numBytes = 0;
+        for (; numBytes < 256 && m != 0; numBytes++) {
+            m = m >> 8;
+        }
+
+        const topSixBits: u8 = u8(numBytes - 4);
+        const lengthByte: u8 = topSixBits << 2 + 3;
+
+        Bytes.putUint<u8>(bytesBuffer, lengthByte, BIT_LENGTH.INT_8);
+        Bytes.putUint<u64>(o, i64(i), BIT_LENGTH.INT_64);
+
+        Bytes.copy(o.slice(0, numBytes), bytesBuffer, 1);
+
+        return numBytes + 1;
     }
 
     static decodeInt (input: u8[]): i64 {
@@ -58,7 +87,7 @@ export class Bytes {
         const byteLen = u8(topSixBits) + 4;
 
         const buf = new Array<u8>(byteLen);
-        Bytes.copyToPosition(input, buf);
+        Bytes.copy(input, buf);
 
         if (byteLen == 4) {
             return u64(Bytes.toUint<u32>(buf, BIT_LENGTH.INT_32));
@@ -66,7 +95,7 @@ export class Bytes {
 
         if (byteLen > 4 && byteLen < 8) {
             const tmp = new Array<u8>(8);
-            Bytes.copyToPosition(buf, tmp);
+            Bytes.copy(buf, tmp);
             return Bytes.toUint<i64>(tmp, BIT_LENGTH.INT_64);
         }
 
